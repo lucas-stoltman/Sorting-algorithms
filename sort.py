9bc872d8a522f5524fed416ac68b3a15565b49c8
RainyKnight <stoltmanlucas@gmail.com>
2022-05-09 19:43:41 -0700

    Basic skeleton

---
+++ sort.py
@@ -0,0 +1,30 @@
+# Created by Lucas Stoltman
+# Program 5
+# May 9th, 2022
+# CSS 340 A, Dimpsey
+# Version: 1.0
+
+
+# Bubble Sort
+def bubble_sort(arr: list):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+# TODO Insertion Sort
+def insertion_sort():
+
+# TODO Merge Sort
+def marge_sort():
+
+# TODO Non-Recursive, one extra list Merge Sort (We’ll call this improved version,
+# IterativeMergeSort from here on out in this homework)
+def iterative_merge_sort():
+
+# TODO Quick Sort
+def quick_sort():
+
+# TODO Shell Sort
+def shell_sort():
---
+++ sorter.py
@@ -0,0 +1,7 @@
+# Created by Lucas Stoltman
+# Program 5
+# May 9th, 2022
+# CSS 340 A, Dimpsey
+# Version: 1.0
+
+# Driver
