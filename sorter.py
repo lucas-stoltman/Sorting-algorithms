6e8cf0f27d5534102e1c13f06cfb41e2e7df3930
RainyKnight <stoltmanlucas@gmail.com>
2022-05-13 20:11:22 -0700

    Quick sorter.py implementation

--- sorter.py
+++ sorter.py
@@ -6,9 +6,54 @@
 
 import sort
 import sys
+import random
 
-sort_type = str(sys.argv[1])
-length = eval(sys.argv[2])
+# default values
+sort_type = ""
+length = 10
+display = False
+
+# TODO validate string input
+# sort type
+if len(sys.argv) >= 2:
+    sort_type = str(sys.argv[1])
+
+# TODO validate numeric input
+# list length
+if len(sys.argv) >= 3:
+    length = eval(sys.argv[2])
 
 # PRINT (optional)
-display = str(sys.argv[3])
+if len(sys.argv) == 4:
+    if sys.argv[3] == "PRINT":
+        display = True
+
+# Initialize a random list
+arr = []
+for i in range(length):
+    arr.append(random.randint(0, 100))
+
+
+def sort(s_type: ""):
+    import sort
+    global arr
+    if sort_type == "bubble":
+        return sort.bubble_sort(arr)
+    elif sort_type == "insertion":
+        return sort.insertion_sort(arr)
+    elif sort_type == "merge":
+        return sort.merge_sort(arr)
+    elif sort_type == "imerge":
+        return sort.iterative_merge_sort(arr)
+    elif sort_type == "quick":
+        return sort.quick_sort(arr)
+    elif sort_type == "shell":
+        return sort.shell_sort(arr)
+
+
+if display:
+    print("Before\t", arr)
+    sort(sort_type)
+    print("After\t", arr)
+else:
+    sort(sort_type)
